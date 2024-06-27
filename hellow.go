5def91a9d325161d55e7de71222e7812ca1e8670
kanatawa <nofu666@gmail.com>
2024-06-27 16:08:20 +0800

    所有

--- hellow.go
+++ hellow.go
@@ -1,26 +1,38 @@
-// crawler.go
 package main
 
-import (
-	"fmt"
-	"net/http"
-	"sync"
-)
+import "fmt"
 
 func main() {
-	var wg sync.WaitGroup
-	urls := []string{"http://example.com", "http://example.org", "http://example.net"}
-	for _, url := range urls {
-		wg.Add(1)
-		go func(url string) {
-			defer wg.Done()
-			resp, err := http.Get(url)
-			if err != nil {
-				fmt.Println(err)
-				return
-			}
-			fmt.Println(url, resp.Status)
-		}(url)
-	}
-	wg.Wait()
+	/*
+			fmt.Println(3)
+			fmt.Println(4.117)
+			fmt.Println(false)
+			fmt.Println('g')
+			fmt.Println("測試")
+
+		var x int
+		x = 4
+		fmt.Println(x)
+		x = 9
+		fmt.Println(x)
+		var g float64 = 4.7841
+		fmt.Println(g)
+		var n string = "你好"
+		fmt.Println(n)
+		var t bool = false
+		fmt.Println(t)
+		var c rune = 's'
+		fmt.Println(c)
+	*/
+	fmt.Println(3, "lo", 878)
+	var x, y, z, s int
+	//var y int
+	//var z int
+	//var s int
+	fmt.Print("輸入1個數字：")
+	fmt.Scanln(&x, &y, &z)
+	fmt.Print("輸入2個數字：")
+	fmt.Scanln(&s)
+	var result int = x*(y+z) - s
+	fmt.Println(result)
 }
